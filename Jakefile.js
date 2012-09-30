5caf7e6f9c229f67a7701d77ffeaa2062fee9004
Phil Booth <pmbooth@gmail.com>
2012-09-30 20:59:09 +0100

    Initial project structure.

---
+++ Jakefile.js
@@ -0,0 +1,77 @@
+/*jshint nomen:false */
+/*globals require, console, complete, __dirname, process */
+
+(function () {
+    'use strict';
+
+    var exec = require('child_process').exec,
+
+    commands = {
+        minify: './node_modules/.bin/uglifyjs --no-copyright --lift-vars --output ./src/complexityReport.min.js ./src/complexityReport.js',
+        test: './node_modules/.bin/mocha --ui tdd --reporter spec --colors --slow 50 ./test/complexityReport.js',
+        lint: './node_modules/.bin/jshint ./src/complexityReport.js --config config/jshint.json',
+        prepare: 'npm install'
+    };
+
+    desc('Minify the source code for deployment.');
+    task('minify', [ 'prepare', 'lint', 'test' ], function () {
+        runTask(minify, 'Minifying...');
+    }, {
+        async: true
+    });
+
+    desc('Run the unit tests.');
+    task('test', [ 'prepare' ], function () {
+        runTask(test, 'Testing...');
+    }, {
+        async: true
+    });
+
+    desc('Lint the source code.');
+    task('lint', [ 'prepare' ], function () {
+        runTask(lint, 'Linting...');
+    }, {
+        async: true
+    });
+
+    desc('Install dependencies.');
+    task('prepare', function () {
+        runTask(prepare, 'Preparing the build environment...');
+    }, {
+        async: true
+    });
+
+    function runTask (operation, message) {
+        console.log(message);
+        operation();
+    }
+
+    function minify () {
+        runCommand(commands.minify);
+    }
+
+    function test () {
+        runCommand(commands.test);
+    }
+
+    function lint () {
+        runCommand(commands.lint);
+    }
+
+    function prepare () {
+        runCommand(commands.prepare);
+    }
+
+    function runCommand (command) {
+        exec(command, { cwd: __dirname }, function (error, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            if (typeof error === 'object' && error !== null) {
+                console.log(error.message);
+                process.exit(1);
+            }
+            complete();
+        });
+    }
+}());
+
