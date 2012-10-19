b95f23985bba30750dbc3c5517f414e510328d6e
Phil Booth <pmbooth@gmail.com>
2012-10-19 08:47:02 +0100

    Start calculating complexity.

--- src/complexityReport.js
+++ src/complexityReport.js
@@ -3,7 +3,11 @@
 (function () {
     'use strict';
 
-    var check, esprima;
+    var check, esprima,
+
+    syntaxHandlers = {
+        IfStatement: processCondition
+    };
 
     require('coffee-script');
 
@@ -13,18 +17,62 @@
     exports.run = run;
 
     function run (source) {
-        var ast;
+        var ast, report = createReport();
 
-        check.verifyUnemptyString(source);
+        check.verifyUnemptyString(source, 'Invalid source');
 
         ast = esprima.parse(source, {
             loc: true
         });
 
-        return {};
+        //console.dir(ast);
+
+        processAst(ast.body, report);
+
+        return report;
     }
 
-    function processFunction (fn) {
+    function createReport () {
+        return {
+            aggregate: createFunctionReport()
+        };
+    }
+
+    function createFunctionReport (name) {
+        return {
+            name: name,
+            complexity: {
+                cyclomatic: 1
+            }
+        };
+    }
+
+    function processAst (ast, report) {
+        var i;
+
+        check.verifyArray(ast, 'Invalid syntax tree');
+
+        for (i = 0; i < ast.length; i += 1) {
+            check.verifyObject(ast[i], 'Invalid node');
+
+            if (check.isFunction(syntaxHandlers[ast[i].type])) {
+                syntaxHandlers[ast[i].type](ast[i], report);
+            }
+        }
+    }
+
+    function processCondition (condition, report) {
+        report.aggregate.complexity.cyclomatic += 1;
+
+        console.dir(condition.test);
+        console.dir(condition.consequent);
+        console.dir(condition.alternate);
+    }
+
+    function processBlock (block, report) {
+    }
+
+    function processFunction (fn, report) {
     }
 }());
 
--- test/complexityReport.js
+++ test/complexityReport.js
@@ -60,6 +60,74 @@
             test('run returns object', function () {
                 assert.isObject(cr.run('"foo"'));
             });
+
+            test('run returns aggregate complexity property', function () {
+                assert.isObject(cr.run('"foo"').aggregate.complexity);
+            });
+
+            suite('run against function call:', function () {
+                var report;
+
+                setup(function () {
+                    report = cr.run('parseInt("10", 10);');
+                });
+
+                teardown(function () {
+                    report = undefined;
+                });
+
+                test('aggregate cyclomatic complexity is 1', function () {
+                    assert.strictEqual(report.aggregate.complexity.cyclomatic, 1);
+                });
+            });
+
+            suite('run against function definition:', function () {
+                var report;
+
+                setup(function () {
+                    report = cr.run('function foo () { "bar"; }');
+                });
+
+                teardown(function () {
+                    report = undefined;
+                });
+
+                test('cyclomatic complexity is 1', function () {
+                    assert.strictEqual(report.aggregate.complexity.cyclomatic, 1);
+                });
+            });
+
+            suite('run against assignment:', function () {
+                var report;
+
+                setup(function () {
+                    report = cr.run('var foo = "bar";');
+                });
+
+                teardown(function () {
+                    report = undefined;
+                });
+
+                test('cyclomatic complexity is 1', function () {
+                    assert.strictEqual(report.aggregate.complexity.cyclomatic, 1);
+                });
+            });
+
+            suite('run against condition:', function () {
+                var report;
+
+                setup(function () {
+                    report = cr.run('if (true) { "foo"; }');
+                });
+
+                teardown(function () {
+                    report = undefined;
+                });
+
+                test('cyclomatic complexity is 2', function () {
+                    assert.strictEqual(report.aggregate.complexity.cyclomatic, 2);
+                });
+            });
         });
     });
 }());
