a7e852de9f20df8fea2a69bdc3fc23f47a293abe
Phil Booth <pmbooth@gmail.com>
2012-10-27 15:09:41 +0100

    Initial implementation of plain formatter.

---
+++ src/formats/plain.js
@@ -0,0 +1,46 @@
+/*globals exports */
+
+'use strict';
+
+exports.format = format;
+
+function format (reports) {
+    var formatted = '', i;
+
+    for (i = 0; i < reports.length; i += 1) {
+        formatted += formatModule(reports[i]);
+    }
+}
+
+function formatModule (report) {
+    return [
+        '\n',
+        report.module,
+        '\n\n',
+        'Aggregate complexity: ',
+        report.aggregate.complexity.cyclomatic,
+        '\n\n',
+        formatFunctions(report.functions)
+    ].join('');
+}
+
+function formatFunctions (report) {
+    var formatted = '', i;
+
+    for (i = 0; i < report.length; i += 1) {
+        formatted += '\n\n' + formatFunction(report[i]);
+    }
+
+    return formatted;
+}
+
+function formatFunction (report) {
+    return [
+        'Function: ',
+        report.name,
+        '\n'
+        'Cyclomatic complexity: ',
+        report.complexity.cyclomatic
+    ].join('');
+}
+
